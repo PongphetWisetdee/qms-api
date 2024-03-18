8f0ba721002a2cd5a3f4ce124c438b1b3a64a7e7
PongphetWisetdee <pongphet.wisetdee@gmail.com>
2024-03-18 11:07:53 +0700

    clean code

--- src/main/java/com/qms/repository/ShopRepository.java
+++ src/main/java/com/qms/repository/ShopRepository.java
@@ -4,8 +4,6 @@ import com.qms.entity.ShopEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface ShopRepository extends CrudRepository<ShopEntity, String> {
     ShopEntity findByShopId(String shopId);
